2119238246964809f0304fe98bcb8cd4d248d814
buffer <knefyodov@bk.ru>
2022-02-13 12:27:09 +0300

    Moving of player

--- 1.py
+++ 1.py
@@ -8,6 +8,11 @@ size = width, height = 1152, 648
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 i = 0
+MOVE_SPEED = 4
+JUMP_POWER = 10
+GRAVITY_FORCE = 0.55
+PLAYER_WIDTH = 18
+PLAYER_HEIGHT = 35
 
 
 all_sprites = pygame.sprite.Group()
@@ -55,18 +60,20 @@ class Box(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y):
-        self.is_ground = False
         self.jumped = False
-        self.x = 0
-        self.y = 0
+        self.xvel = 0
+        self.yvel = 0
+        self.isGround = False
         super().__init__(all_sprites)
+        self.rect = pygame.Rect(x, y, 0, 0)
         self.frames = []
         self.cut_sheet(sheet, columns, rows)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
 
+
     def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(self.x, self.y, sheet.get_width() // columns,
+        self.rect = pygame.Rect(self.rect.x, self.rect.y, sheet.get_width() // columns,
                                 sheet.get_height() // rows)
         for j in range(rows):
             for i in range(columns):
@@ -74,17 +81,28 @@ class Player(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
-    def update(self):
-        if self.jumped:
-            self.rect.y -= self.coef
-            self.y -= self.coef
-            self.coef -= 1
-        if not pygame.sprite.spritecollideany(self, boxes):
-            self.rect.y += 5
-            self.y += 5
-        else:
-            self.is_ground = True
-            self.jumped = False
+    def update(self, left, right, up):
+
+        if right:
+            self.xvel = MOVE_SPEED
+        if left:
+            self.xvel = -MOVE_SPEED
+        if not (left or right):
+            self.xvel = 0
+        if up:
+            if self.isGround:
+                self.yvel = -JUMP_POWER
+        if not self.isGround:
+            self.yvel += GRAVITY_FORCE
+
+        self.rect.x += self.xvel
+        # self.x += self.xvel
+        self.collide(self.xvel, 0, boxes)
+
+        self.isGround = False
+        self.rect.y += self.yvel
+        # self.y += self.yvel
+        self.collide(0, self.yvel, boxes)
         global i
         if i == 10:
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
@@ -98,6 +116,28 @@ class Player(pygame.sprite.Sprite):
             self.coef = 18
             self.jumped = True
             self.is_ground = False
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def collide(self, xvel, yvel, boxes):
+        for box in boxes:
+            if pygame.sprite.collide_rect(self, box):  # если есть пересечение платформы с игроком
+
+                if xvel > 0:  # если движется вправо
+                    self.rect.right = box.rect.left  # то не движется вправо
+
+                if xvel < 0:  # если движется влево
+                    self.rect.left = box.rect.right  # то не движется влево
+
+                if yvel > 0:  # если падает вниз
+                    self.rect.bottom = box.rect.top  # то не падает вниз
+                    self.isGround = True  # и становится на что-то твердое
+                    self.yvel = 0  # и энергия падения пропадает
+
+                if yvel < 0:  # если движется вверх
+                    self.rect.top = box.rect.bottom  # то не движется вверх
+                    self.yvel = 0
 
 
 moving = False
@@ -128,6 +168,7 @@ def main():
     player = Player(load_image('stay.png'), 4, 1, 200, 200)
     generate_level(load_level('lvl1.txt'))
     running = True
+    left = right = up = False
     # fon = Fon()
     while running:
         for event in pygame.event.get():
@@ -135,34 +176,35 @@ def main():
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    moving = 4
+                    right = True
                     player.frames = []
                     i = 10
                     player.cut_sheet(load_image("run.png"), 6, 1)
                 elif event.key == pygame.K_LEFT:
-                    moving = -4
+                    left = True
                     player.frames = []
                     i = 10
                     player.cut_sheet(load_image("run1.png"), 6, 1)
                 elif event.key == pygame.K_UP:
-                    player.jump()
+                    up = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
+                    right = False
                     moving = False
                     player.frames = []
                     player.cut_sheet(load_image("stay.png"), 4, 1)
                 elif event.key == pygame.K_LEFT:
+                    left = False
                     moving = False
                     player.frames = []
                     player.cut_sheet(load_image("stay1.png"), 4, 1)
+                elif event.key == pygame.K_UP:
+                    up = False
             # elif event.type == pygame.MOUSEBUTTONDOWN:
 
-        if moving:
-            player.x += moving
-            player.rect.x += moving
         screen.blit(bg, (0, 0))
         all_sprites.draw(screen)
-        all_sprites.update()
+        all_sprites.update(left, right, up)
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
